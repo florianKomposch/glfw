f4b09011f50eb3c0198c4fdd2cd4d6ea3ca66774
Florian Komposch <florian.komposch@student.tugraz.at>
2022-10-26 17:13:22 +0200

    reverted file to first iteration

--- premake5.lua
+++ premake5.lua
@@ -26,67 +26,30 @@ project "GLFW"
 		"src/window.c",
 	}
 
-	filter "system:linux"
-		pic "On"
-
-		systemversion "latest"
-		
-		files
-		{
-			"src/x11_init.c",
-			"src/x11_monitor.c",
-			"src/x11_window.c",
-			"src/xkb_unicode.c",
-			"src/posix_time.c",
-			"src/posix_thread.c",
-			"src/glx_context.c",
-			"src/egl_context.c",
-			"src/osmesa_context.c",
-			"src/linux_joystick.c"
-		}
-
-		defines
-		{
-			"_GLFW_X11"
-		}
-
 	filter "system:windows"
-		systemversion "latest"
+	buildoptions {"-std=c11", "-lgdi32"}
+	systemversion "latest"
+	staticruntime "On"
 
-		files
-		{
-			"src/win32_init.c",
-			"src/win32_joystick.c",
-			"src/win32_module.c",
-			"src/win32_monitor.c",
-			"src/win32_time.c",
-			"src/win32_thread.c",
-			"src/win32_window.c",
-			"src/wgl_context.c",
-			"src/egl_context.c",
-			"src/osmesa_context.c"
-		}
+	files
+	{
+		"src/win32_init.c",
+		"src/win32_joystick.c",
+		"src/win32_module.c",
+		"src/win32_monitor.c",
+		"src/win32_time.c",
+		"src/win32_thread.c",
+		"src/win32_window.c",
+		"src/wgl_context.c",
+		"src/egl_context.c",
+		"src/osmesa_context.c"
+	}
 
-		defines 
-		{ 
-			"_GLFW_WIN32",
-			"_CRT_SECURE_NO_WARNINGS"
-		}
+	defines 
+	{ 
+		"_GLFW_WIN32",
+		"_CRT_SECURE_NO_WARNINGS"
+	}
 
-		links
-		{
-			"Dwmapi.lib"
-		}
-
-	filter "configurations:Debug"
-		runtime "Debug"
-		symbols "on"
-
-	filter "configurations:Release"
-		runtime "Release"
-		optimize "on"
-
-	filter "configurations:Dist"
-		runtime "Release"
-		optimize "on"
-        symbols "off"
+	filter { "system:windows", "configuration:Release"}
+		buildoptions "/MT"
